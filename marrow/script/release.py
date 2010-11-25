044eecbff3f15763bdbefc60db839aef0f46900c
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-11-24 17:28:20 -0800

    Updated description for pypi.

--- marrow/script/release.py
+++ marrow/script/release.py
@@ -8,11 +8,23 @@ version = "1.0"
 release = "1.0"
 
 summary = "Turn any callable into a powerful command line script through arglist introspection."
-description = """The ``marrow.script`` package is a small library for turning average every-day callables (such as functions and class methods) into command-line scripts while automatically determining argument naming, typecasting, and generating things like help and version information.  All behavior can be overridden by you, the developer, giving you a flexible and easy to develop with command line parsing library to replace ``optparse`` and ``argparse``.  This package is not a wrapper around existing parsing libraries, and attempts to match the syntax common to GNU software.
+description = """The marrow.script package is a small library for turning average every-day
+callables (such as functions and class methods) into command-line scripts
+while automatically determining argument naming, typecasting, and generating
+things like help and version information.  All behavior can be overridden by
+you, the developer, giving you a flexible and easy to develop with command
+line parsing library to replace ``optparse`` and ``argparse``.  This package
+is not a wrapper around existing parsing libraries, and attempts to match the
+syntax common to GNU software.
 
-In a larger scope ``marrow.script`` aims to replace other high-level command-line scripting libraries such as Paste Script and ``commandline`` while also implementing Python 3 compatibility.
+In a larger scope marrow.script aims to replace other high-level command-line
+scripting libraries such as Paste Script and commandline while also
+implementing Python 3 compatibility.
 
-For full documentation, see the README.textile file present in the package, or view it online on the `GitHub project page<https://github.com/pulp/marrow.script>`_."""
+For full documentation, see the README.textile file present in the package,
+or view it online on the GitHub project page:
+
+https://github.com/pulp/marrow.script"""
 author = "Alice Bevan-McGregor"
 email = "alice@gothcandy.com"
 url = "https://github.com/pulp/marrow.script"
