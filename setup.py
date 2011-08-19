1754e575a1b33082d3e2b270723026d2b9fde3a0
Alice Bevan-McGregor <alice@gothcandy.com>
2011-08-19 19:18:26 -0400

    Unification of release information, setup script, and setup configuration.

--- setup.py
+++ setup.py
@@ -1,35 +1,16 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import sys, os
-
-try:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-
-except ImportError:
-    pass
+import os
+import sys
 
 from setuptools import setup, find_packages
 
 
-if sys.version_info <= (2, 6):
+if sys.version_info < (2, 6):
     raise SystemExit("Python 2.6 or later is required.")
 
-if sys.version_info >= (3,0):
-    def execfile(filename, globals_=None, locals_=None):
-        if globals_ is None:
-            globals_ = globals()
-        
-        if locals_ is None:
-            locals_ = globals_
-        
-        exec(compile(open(filename).read(), filename, 'exec'), globals_, locals_)
-
-else:
-    from __builtin__ import execfile
-
-execfile(os.path.join("marrow", "script", "release.py"), globals(), locals())
+exec(open(os.path.join("marrow", "script", "release.py")))
 
 
 
@@ -38,7 +19,8 @@ setup(
         version = version,
         
         description = "Turn any callable into a powerful command line script through arglist introspection.",
-        long_description = """The marrow.script package is a small library for turning average every-day
+        long_description = """\
+The marrow.script package is a small library for turning average every-day
 callables (such as functions and class methods) into command-line scripts
 while automatically determining argument naming, typecasting, and generating
 things like help and version information.  All behavior can be overridden by
@@ -55,15 +37,15 @@ For full documentation, see the README.textile file present in the package,
 or view it online on the GitHub project page:
 
 https://github.com/marrow/marrow.script""",
+        
         author = "Alice Bevan-McGregor",
         author_email = "alice+marrow@gothcandy.com",
         url = "https://github.com/marrow/marrow.script",
-        download_url = "http://pypi.python.org/pypi/marrow.script",
         license = "MIT",
-        keywords = '',
         
-        use_2to3 = True,
-        install_requires = ['marrow.util'],
+        install_requires = [
+            'marrow.util < 2.0'
+        ],
         
         test_suite = 'nose.collector',
         tests_require = [
@@ -87,15 +69,10 @@ https://github.com/marrow/marrow.script""",
                 "Topic :: Utilities"
             ],
         
-        packages = find_packages(exclude=['examples', 'tests', 'tests.*', 'docs', 'third-party']),
-        include_package_data = True,
-        package_data = {
-                '': ['README.textile', 'LICENSE', 'distribute_setup.py'],
-                'docs': ['Makefile', 'source/*']
-            },
+        packages = find_packages(exclude=['examples', 'tests']),
         zip_safe = True,
+        include_package_data = True,
+        package_data = {'': ['README.textile', 'LICENSE']},
         
         namespace_packages = ['marrow'],
-        
-        entry_points = {}
     )
