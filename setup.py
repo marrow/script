d1d9459dd4860fbce91a9e815bfc5e8d01b9c56d
Alice Bevan-McGregor <alice@gothcandy.com>
2016-10-23 00:34:19 -0400

    Project metadata template update.

--- setup.py
+++ setup.py
@@ -13,81 +13,75 @@ try:
 except ImportError:
 	from setuptools import setup, find_packages
 
-from setuptools.command.test import test as TestCommand
-
 
 if sys.version_info < (2, 7):
 	raise SystemExit("Python 2.7 or later is required.")
 elif sys.version_info > (3, 0) and sys.version_info < (3, 2):
-	raise SystemExit("Python 3.2 or later is required.")
+	raise SystemExit("CPython 3.3 or Pypy 3 (3.2) or later is required.")
 
-exec(open(os.path.join("marrow", "script", "release.py")).read())
-
-
-class PyTest(TestCommand):
-	def finalize_options(self):
-		TestCommand.finalize_options(self)
-		
-		self.test_args = []
-		self.test_suite = True
-	
-	def run_tests(self):
-		import pytest
-		sys.exit(pytest.main(self.test_args))
-
+version = description = url = author = author_email = ""  # Silence linter warnings.
+exec(open(os.path.join("marrow", "script", "release.py")).read())  # Actually populate those values.
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-tests_require = ['pytest', 'pytest-cov', 'pytest-flakes', 'pytest-spec']
+tests_require = [
+		'pytest',  # test collector and extensible runner
+		'pytest-cov',  # coverage reporting
+		'pytest-flakes',  # syntax validation
+		'pytest-capturelog',  # log capture
+	]
+
 
 setup(
 	name = "marrow.script",
 	version = version,
-	
 	description = description,
 	long_description = codecs.open(os.path.join(here, 'README.rst'), 'r', 'utf8').read(),
 	url = url,
-	
+	download_url = 'https://github.com/marrow/cinje/releases',
 	author = author.name,
 	author_email = author.email,
-	
 	license = 'MIT',
-	keywords = '',
+	keywords = ['template', 'source translation', 'dsl', 'streaming', 'chunked'],
 	classifiers = [
 			"Development Status :: 5 - Production/Stable",
+			"Environment :: Console",
 			"Intended Audience :: Developers",
 			"License :: OSI Approved :: MIT License",
 			"Operating System :: OS Independent",
-			"Programming Language :: Python",
 			"Programming Language :: Python :: 2",
 			"Programming Language :: Python :: 2.7",
 			"Programming Language :: Python :: 3",
 			"Programming Language :: Python :: 3.2",
 			"Programming Language :: Python :: 3.3",
 			"Programming Language :: Python :: 3.4",
+			"Programming Language :: Python :: 3.5",
 			"Programming Language :: Python :: Implementation :: CPython",
 			"Programming Language :: Python :: Implementation :: PyPy",
+			"Programming Language :: Python",
 			"Topic :: Software Development :: Libraries :: Python Modules",
-			"Topic :: Utilities"
+			"Topic :: Software Development :: Libraries",
+			"Topic :: Utilities",
 		],
 	
-	packages = find_packages(exclude=['test', 'script', 'example']),
+	packages = find_packages(exclude=['bench', 'docs', 'example', 'test', 'htmlcov']),
 	include_package_data = True,
-	namespace_packages = ['marrow'],
-	
-	install_requires = [],
-	
-	extras_require = dict(
-			development = tests_require,
-		),
-	
-	tests_require = tests_require,
-	
-	dependency_links = [
+	package_data = {'': ['README.rst', 'LICENSE.txt']},
+	namespace_packages = [
+			'marrow',
 		],
-	
 	zip_safe = True,
-	cmdclass = dict(
-			test = PyTest,
-		)
+	
+	entry_points = {
+		},
+	
+	setup_requires = [
+			'pytest-runner',
+		] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else [],
+	install_requires = [],
+	tests_require = tests_require,
+	extras_require = {
+			'development': tests_require,  # Development requirements are the testing requirements.
+		},
 )
+
