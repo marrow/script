be643aab1f882ae3ca2cf0351219fdb74e340417
Alice Bevan-McGregor <alice@gothcandy.com>
2016-10-23 00:38:34 -0400

    Version bump, added Makefile.

--- marrow/script/release.py
+++ marrow/script/release.py
@@ -5,10 +5,11 @@
 from collections import namedtuple
 
 
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(1, 0, 0, 'final', 1)
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(2, 0, 0, 'beta', 1)
 version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
 
 author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
 
 description = "Turn any callable into a powerful command line script through arglist introspection."
 url = 'https://github.com/marrow/script/'
+
